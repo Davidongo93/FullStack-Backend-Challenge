5d10355f24641707f038e44de0c666b6f6b1c008
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-01-20 15:28:24 -0500

    Feat: add parsers App middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
 import morganMiddleware from "./middleware/morganMiddleware";
 import errorHandlerMiddleware from "./middleware/errorHandlingMiddleware";
-import cors from "cors";
+import configureBodyParsers from "./middleware/bodyParsersMiddleware";
+import configureCookieParser from "./middleware/cookieParserMiddleware";
 
 const app: Express = express();
 
-//Logging Middleware
+// Logging Middleware
 app.use(morganMiddleware());
-
+// This is CORS-enabled for all origins!
 app.use(cors());
+// Configure body parsers and cookie parsers.
+configureBodyParsers(app);
+configureCookieParser(app);
 
 app.get('/', (req:Request, res:Response) => {
     res.send('Hello World!')
