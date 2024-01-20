c958922fc1f3b617ea2681869b9b4469edca6feb
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-01-20 15:15:14 -0500

    Feat: Add error handling endware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import morganMiddleware from "./middleware/morganMiddleware";
+import errorHandlerMiddleware from "./middleware/errorHandlingMiddleware";
+import cors from "cors";
 
 const app: Express = express();
 
+//Logging Middleware
 app.use(morganMiddleware());
+
+app.use(cors());
 
 app.get('/', (req:Request, res:Response) => {
     res.send('Hello World!')
-  })
+  });
+
+// Error Catching Endware
+app.use(errorHandlerMiddleware);
 
 
 
