a124810e09beef0729f63b244a2e8116ba7e8fe0
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-01-20 14:58:48 -0500

    Feat: add Morgan middleware for logging

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response, NextFunction } from "express";
+import morganMiddleware from "./middleware/morganMiddleware";
 
 const app: Express = express();
 
-app.get('/', (req, res) => {
+app.use(morganMiddleware());
+
+app.get('/', (req:Request, res:Response) => {
     res.send('Hello World!')
   })
 
